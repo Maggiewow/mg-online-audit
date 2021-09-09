51a939cd7840c19c71da89b3e8b94357e4750cc6
Maggie <13256724140@163.com>
2021-09-09 14:30:00 +0800

    修改babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-08-12 18:18:35
- * @LastEditTime: 2021-08-10 18:09:08
+ * @LastEditTime: 2021-09-09 14:28:22
  * @LastEditors: 赵婷婷
  * @Description: In User Settings Edit
  * @FilePath:
  */
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset',
-    ["@babel/preset-env", { "modules": false }]
-  ],
+  presets: ['@vue/cli-plugin-babel/preset'],
   plugins: [
     [
       'import',
@@ -19,13 +16,29 @@ module.exports = {
         libraryDirectory: 'src/components',
       },
     ],
-    [
-      "component",
-      {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
-    ]
   ],
-  sourceType: 'unambiguous',
 };
+
+// module.exports = {
+//   presets: [
+//     '@vue/cli-plugin-babel/preset',
+//     ["@babel/preset-env", { "modules": false }]
+//   ],
+//   plugins: [
+//     [
+//       'import',
+//       {
+//         libraryName: 'view-design',
+//         libraryDirectory: 'src/components',
+//       },
+//     ],
+//     [
+//       "component",
+//       {
+//         "libraryName": "element-ui",
+//         "styleLibraryName": "theme-chalk"
+//       }
+//     ]
+//   ],
+//   sourceType: 'unambiguous',
+// };
