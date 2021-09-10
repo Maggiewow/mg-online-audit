261f2043d82072e6509f4c667fec12d1344d56f7
Maggie <13256724140@163.com>
2021-09-10 17:42:01 +0800

    babel-plugin-transform-es2015-modules-commonjs

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@
  * 问题是，如果 Babel 认为某个东西是 ES 模块，它会插入import语句。如果您将import语句插入到也使用 CommonJS 之类module.exports的文件中，
  * 则意味着该文件现在将在同一个文件中使用两个模块系统，这是一个大问题，并会导致您看到的错误
  * @Date: 2020-08-12 18:18:35
- * @LastEditTime: 2021-09-10 15:20:53
+ * @LastEditTime: 2021-09-10 17:18:36
  * @LastEditors: 赵婷婷
  * @Description: In User Settings Edit
  * @FilePath:
@@ -14,10 +14,12 @@
 module.exports = {
   presets: [
     // '@vue/cli-plugin-babel/preset',
+    '@babel/preset-react',
     ['@babel/preset-env', { modules: false, useBuiltIns: 'usage' }],
   ],
-  plugins: [],
+  // plugins: [],
   // plugins: ['transform-vue-jsx', 'transform-runtime', 'transform-es2015-modules-commonjs'],
+  plugins: ['@babel/plugin-transform-runtime', 'transform-es2015-modules-commonjs'],
   // plugins: [
   //   [
   //     'import',
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-24 09:12:36
- * @LastEditTime: 2021-09-08 17:55:47
+ * @LastEditTime: 2021-09-10 17:32:06
  * @LastEditors: 赵婷婷
  * @Description: In User Settings Edit
  * @FilePath: \sucai-modal\vue.config.js
@@ -15,6 +15,7 @@ var webpack = require('webpack');
 // const BASE_URL = process.env.NODE_ENV === 'production' ? '/dist/' : './';
 
 module.exports = {
+  // export default {
   // publicPath: BASE_URL,
   pages: {
     index: {
